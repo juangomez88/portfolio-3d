e7c32e7d612b2ee1a8d467e372b14bb5f50e2cbd
Juan Carlos Gómez <juan.gomez77@udea.edu.co>
2025-03-03 15:38:41 -0500

    Componentes Sky, Bird y Plane funcionales. Se hacen modificaciones a Home y Island

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,57 +1,70 @@
-import { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
-import Loader from '../components/Loader'
-import Island from '../modeles/Island'
-import Sky from '../modeles/Sky'
-import Bird from '../modeles/Bird'
-import Plane from '../modeles/Plane'
-
-{/* <div className=' absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-  POPUP
-</div> */}
+import { Suspense, useState } from "react"
+import { Canvas } from "@react-three/fiber"
+import Loader from "../components/Loader"
+import Island from "../modeles/Island"
+import Sky from "../modeles/Sky"
+import Bird from "../modeles/Bird"
+import Plane from "../modeles/Plane"
 
 const Home = () => {
+  const [isRotating, setIsRotating] = useState(false)
+  const [currentStage, setCurrentStage] = useState(1)
 
   const adjustIslandForScreenSize = () => {
+    let screenScale = null
+    const screenPosition = [0, -6.5, -43]
+    const rotation = [0.1, 4.7, 0]
 
-    let screenScale = null;
-    let screenPosition= [0, -6.5, -43];
-    let rotation = [0.1, 4.7, 0];
-
-    if(window.innerWidth < 768) {
+    if (window.innerWidth < 768) {
       screenScale = [0.9, 0.9, 0.9]
-      
-    }else {
+    } else {
       screenScale = [1, 1, 1]
     }
     return [screenScale, screenPosition, rotation]
   }
 
-  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+  const adjustPlaneForScreenSize = () => {
+    let screenScale, screenPosition
+
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5]
+      screenPosition = [0, -1.5, 0]
+    } else {
+      screenScale = [3, 3, 3]
+      screenPosition = [0, -0.5, -4]
+    }
+    return [screenScale, screenPosition]
+  }
+
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize()
+  const [planeScale, planePosition] = adjustPlaneForScreenSize()
 
   return (
-    <section className=' w-full h-screen relative'>
-      <Canvas 
-        className=' w-full h-screen bg-transparent'
+    <section className="w-full h-screen relative">
+      <Canvas
+        className={`w-full h-screen bg-transparent ${isRotating ? "cursor-grabbing" : "cursor-grab"}`}
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
-          <directionalLight position={[1, 1, 1]} instensity={2}/>
-          <ambientLight instensity={0.5}/>
-          <hemisphereLight 
-            skyColor='#b1e1ff' 
-            groundColor='#000000'
-            instensity={1}
-          />
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
           <Bird />
-          <Sky />
+          <Sky isRotating={isRotating} />
           <Island
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+            setCurrentStage={setCurrentStage}
           />
-
-          <Plane />
+          <Plane 
+            isRotating={isRotating} 
+            scale={planeScale} 
+            position={planePosition} 
+            rotation={[0, 20, 0]} 
+          />
         </Suspense>
       </Canvas>
     </section>
@@ -59,3 +72,4 @@ const Home = () => {
 }
 
 export default Home
+
