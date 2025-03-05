0665795c8a8d70eb84fdf945b55e61d4e64c368c
Juan Carlos Gómez <juan.gomez77@udea.edu.co>
2025-03-05 13:25:51 -0500

    Se crea el componente Contact.jsx y la animacion de zorro en la página

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <header className='header'>
+    <header className=' flex justify-between items-center sm:px-16 px-8 py-4 max-w-5xl mx-auto absolute top-0 bg-transparent z-10 right-0 left-0'>
       <NavLink to="/" className=" w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
         <p className=' text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-indigo-600'>JC</p>
       </NavLink>
